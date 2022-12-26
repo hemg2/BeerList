b0b3436552b085619d57350557c7dab2da538b00
hemg2 <hemg2@naver.com>
2022-12-26 22:04:55 +0900

    style

---
+++ View/BeerListCell.swift
@@ -0,0 +1,15 @@
+//
+//  BeerCell.swift
+//  Brewery
+//
+//  Created by 1 on 2022/12/26.
+//
+
+import UIKit
+
+
+class BeerListCell: UITableViewCell {
+    
+    
+    
+}
--- View/BeerListViewController.swift
+++ View/BeerListViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     
+    var beerList = [Beer]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "할머니 맥주"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+
+extension BeerListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
     }
 }
 

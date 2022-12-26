058282898a207bf94ba41e754e1654a1fa59bb3a
hemg2 <hemg2@naver.com>
2022-12-26 21:58:22 +0900

    test

---
+++ View/BeerListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BeerListViewController.swift
+//  Brewery
+//
+//  Created by 1 on 2022/12/26.
+//
+
+import UIKit
+
+
+class BeerListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "할머니 맥주"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+

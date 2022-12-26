e7461b0bd5905db72a3014238db211d428c41696
hemg2 <hemg2@naver.com>
2022-12-26 21:46:27 +0900

    Model

--- Brewery/Model/Beer.swift
+++ Brewery/Model/Beer.swift
@@ -13,20 +13,19 @@ struct Beer: Decodable {
     let foodParing: [String]?
     
     var tagLine: String {
-        let tags = taglineString?.components(separatedBy: ".")
-        let hashtags = tags?.map {
-            "#" + $0.replacingOccurrences(of: " ", with: "")
-                .replacingOccurrences(of: ".", with: "")
-                .replacingOccurrences(of: ",", with: "#")
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map { "#" + $0.replacingOccurrences(of: " ", with: "")
+            .replacingOccurrences(of: ".", with: "")
+            .replacingOccurrences(of: ",", with: " #")
         }
-        return hashtags?.joined(separator: " ") ?? "" 
+        return hashtags?.joined(separator: " ") ?? ""
     }
     
     enum CodingKeys: String, CodingKey {
-        case id, name, escription
+        case id, name, description
         case taglineString = "tagline"
         case imageURL = "image_url"
         case brewersTips = "brewers_tips"
-        case foodParing = "food_paring"
+        case foodParing = "food_pairing"
     }
 }
